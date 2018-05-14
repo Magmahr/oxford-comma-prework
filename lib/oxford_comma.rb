757076572eefe4e6738a69ef713e80003d909559
Matt Maher <mattmaher0@gmail.com>
2018-05-14 21:06:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
     oxford << array[position] + ", "
     position += 1
   end  
-  oxford << array[-2] + " and " + array[-1]
+  oxford << array[-2] + ", and " + array[-1]
   oxford
 end
