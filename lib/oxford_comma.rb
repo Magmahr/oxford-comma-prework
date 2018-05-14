219b9fad505fd13794858ac4c099c1b975808fc6
Matt Maher <mattmaher0@gmail.com>
2018-05-14 21:07:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
   oxford = ""
   position = 0
+  if array.length < 2
+    oxford << array[position]
+    position += 1
+  end  
   while position < array.length - 2
     oxford << array[position] + ", "
     position += 1
