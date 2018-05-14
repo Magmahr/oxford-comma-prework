8728bdafa579140c409091f1d44d60e95d58a5ef
Matt Maher <mattmaher0@gmail.com>
2018-05-14 21:00:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   oxford = ""
   array.each_with_index do |el, idx|
-    unless idx == idx[-2]
-    oxford << el + ", "
-        oxford << el + "and #{el[idx-1]}"
-      end  
+    if array.length == 1
+      oxford << el
+    elsif array.length == 2
+      oxford << el[0] + " and " el[-1]
+    end
   end  
   oxford
 end
